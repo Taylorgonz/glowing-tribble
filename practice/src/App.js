25ca7df4d583b9fe5a9198d26f651e99b3d1f3ec
taylorgonz <hello@taylorgonz.com>
2021-04-24 10:11:15 -0400

    saturday activities

--- practice/src/App.js
+++ practice/src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
-import FriendCard from './components/FriendCard';
-import Wrapper from './components/Wrapper';
-import Title from './components/Title';
-import friends from './friends.json';
+import FriendCard from "./components/FriendCard";
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
+import friends from "./friends.json";
 
 class App extends Component {
+  // Setting this.state.friends to the friends json array
   state = {
     friends
   };
@@ -16,12 +17,12 @@ class App extends Component {
     this.setState({ friends });
   };
 
-
+  // Map over this.state.friends and render a FriendCard component for each friend object
   render() {
     return (
       <Wrapper>
         <Title>Friends List</Title>
-        {this.state.friends.map(friend => {
+        {this.state.friends.map(friend => (
           <FriendCard
             removeFriend={this.removeFriend}
             id={friend.id}
@@ -31,10 +32,10 @@ class App extends Component {
             occupation={friend.occupation}
             location={friend.location}
           />
-        })}
+        ))}
       </Wrapper>
-    )
-  };
+    );
+  }
 }
 
 export default App;
